0610797e2e831dfaa8710be0bf41fbf2f075b16d
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-05 12:38:34 +0900

    refactoring

---
+++ terminal_gui/controls/listview_sample02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * FileName:     file
+ * Author:       8ucchiman
+ * CreatedDate:  2023-09-19 13:53:00
+ * LastModified: 2023-02-26 13:30:39 +0900
+ * Reference:    https://github.com/gui-cs/Terminal.Gui/issues/152
+ * Description:  ---
+ */
+
+
+using Terminal.Gui;
+
+namespace listview_sample02 {
+    public class Example02 {
+        static void Main(string[] args) {
+            Application.Init();
+            var top = Application.Top;
+
+            top.Add(new MyView());
+
+            // Add some controls
+            Application.Run();
+        }
+    }
+}
---
+++ terminal_gui/controls/listview_sample02/README.md
@@ -0,0 +1,14 @@
+<!--
+ FileName:      readme
+ Author:        8ucchiman
+ CreatedDate:   2023-07-27 13:18:25
+ LastModified:  2023-01-25 10:56:12 +0900
+ Reference:     https://github.com/gui-cs/Terminal.Gui/issues/152
+ Description:   ---
+-->
+
+
+# 
+
+# Reference
+- [issue#152](https://github.com/gui-cs/Terminal.Gui/issues/152)
