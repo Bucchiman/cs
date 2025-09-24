ac403986dff9a606902e47f86b56ce333cf47ba6
8ucchiman <8ucchiman@gmail.com>
2025-09-24 21:48:27 +0900

    update snippet

---
+++ pocket/Bmods.cs
@@ -0,0 +1,22 @@
+/*
+ * FileName:     Bmods
+ * Author:       8ucchiman
+ * CreatedDate:  2023-09-19 13:53:00
+ * LastModified: 2023-02-26 13:30:39 +0900
+ * Reference:    8ucchiman.jp
+ * Description:  ---
+ */
+
+
+
+using System;
+
+namespace Bsystem{
+    class Bmods{
+        private int a = 4;
+        static void Main(string[] argv){
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
+
